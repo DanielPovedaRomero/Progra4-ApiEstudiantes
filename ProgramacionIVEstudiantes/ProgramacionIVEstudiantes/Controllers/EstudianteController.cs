a7f0026c354e093a70a4796ecce0a3554656201d
danielpoveda <danielpovedaromero@gmail.com>
2024-02-02 19:25:58 -0600

    Se elimina clase default del api

--- ProgramacionIVEstudiantes/ProgramacionIVEstudiantes/Controllers/EstudianteController.cs
+++ ProgramacionIVEstudiantes/ProgramacionIVEstudiantes/Controllers/EstudianteController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProgramacionIV.BLL;
+using ProgramacionIV.BLL.Interfaces;
 using ProgramacionIV.ETL;
 
 namespace ProgramacionIVEstudiantes.Controllers
@@ -8,11 +10,20 @@ namespace ProgramacionIVEstudiantes.Controllers
     [Produces("aplication/json")]
     public class EstudianteController : Controller
     {
+
+        private LnEstudiante oLnEstudiante;
+
+        public EstudianteController(IAdEstudiante accesoDatosEstudiante)
+        {
+            oLnEstudiante = new LnEstudiante(accesoDatosEstudiante);
+        }
+
         [HttpPost]
         [Route("InsertarEstudiante")]
         public IActionResult InsertarEstudiante(Estudiante estudiante) 
         {
-            return Ok();
+            int respuesta = oLnEstudiante.InsertarEstudiante(estudiante);
+            return Ok(respuesta);
         }
     }
 }
