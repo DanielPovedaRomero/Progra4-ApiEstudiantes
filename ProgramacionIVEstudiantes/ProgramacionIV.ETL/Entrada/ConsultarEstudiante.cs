062bed91dd2438dbaa97a98e01038f00ba02f6ed
danielpoveda <danielpovedaromero@gmail.com>
2024-02-02 20:00:10 -0600

    Se agrega la clase ConsultarEstudiante

---
+++ ProgramacionIVEstudiantes/ProgramacionIV.ETL/Entrada/ConsultarEstudiante.cs
@@ -0,0 +1,7 @@
+﻿namespace ProgramacionIV.ETL.Entrada
+{
+    public class ConsultarEstudiante
+    {
+        public int numeroCedula { get; set; }
+    }
+}
---
+++ ProgramacionIVEstudiantes/ProgramacionIV.ETL/Salida/ConsultarEstudiante.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgramacionIV.ETL.Salida
+{
+    public class ConsultarEstudiante
+    {
+        public string nombre { get; set; }
+        public string apellido1 { get; set; }
+        public string apellido2 { get; set; }
+        public int edad { get; set; }
+        public string correo { get; set; }
+    }
+}
